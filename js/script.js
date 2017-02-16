5f6a552f3bca031c000798eb96ccb80032c2ab5b
Nikita42 <nikitaostapenko12@gmail.com>
2017-02-16 23:17:19 +0200

    Chenges on the main page

--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
 (function($) {
 	$(document).ready(function() {
 
-		$('.clients').slick({
-		  dots: false,
+	var servicesDrop = $('.dropdown-services'),
+		servicesLi = $('.services');
+
+
+/*=============Sliders part=======================================*/
+		$('.clients-wrap').slick({
+		  dots: true,
+		  arrows: false,
 		  infinite: true,
-		  autoplay: true,
+		  autoplay: false,
 		  autoplaySpeed: 5000,		  
-		  speed: 300,
+		  speed: 500,
 		  slidesToShow: 3,
 		  slidesToScroll: 1,
 		  responsive: [
@@ -35,6 +41,34 @@
 		    }
 		  ]
 		});
+
+		$('.top-slider').slick({
+		  dots: false,
+		  infinite: true,
+		  autoplay: false,
+		  autoplaySpeed: 5000,		  
+		  speed: 500,
+		  slidesToShow: 1,
+		  slidesToScroll: 1
+		});
+
+
+/*====================Events==============================*/		
+
+	servicesLi.mouseover(function(){
+		$(this).find(' > a').css({
+    		'background' : '#0FBE7C',
+    		'color' : '#fff'
+		});
+		servicesDrop.stop().fadeIn(200);
+	});
+	servicesLi.mouseout(function(){
+		$(this).find(' > a').css({
+    		'background' : 'none',
+    		'color' : '#333'
+		});
+		servicesDrop.stop().fadeOut(200);
+	});
 
 	});
 }) (jQuery);		
