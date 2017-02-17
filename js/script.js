96cdc781b50cf487f04d20a7d1c5a554b370b3b2
Nikita42 <nikitaostapenko12@gmail.com>
2017-02-17 21:10:17 +0200

    Add page contact and about us

--- js/script.js
+++ js/script.js
@@ -12,17 +12,17 @@
 		  infinite: true,
 		  autoplay: false,
 		  autoplaySpeed: 5000,		  
-		  speed: 500,
+		  speed: 900,
 		  slidesToShow: 3,
-		  slidesToScroll: 1,
+		  slidesToScroll: 2,
 		  responsive: [
 		    {
 		      breakpoint: 1024,
 		      settings: {
 		        slidesToShow: 3,
-		        slidesToScroll: 1,
+		        slidesToScroll: 2,
 		        infinite: true,
-		        dots: false
+		        dots: true
 		      }
 		    },
 		    {
@@ -65,10 +65,14 @@
 	servicesLi.mouseout(function(){
 		$(this).find(' > a').css({
     		'background' : 'none',
-    		'color' : '#333'
+    		'color' : '#342e33'
 		});
 		servicesDrop.stop().fadeOut(200);
 	});
+
+	$('.company-img').click(function(){
+		$(this).attr('src', '../img/baron2.jpg');
+	})
 
 	});
 }) (jQuery);		
